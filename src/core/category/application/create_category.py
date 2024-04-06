13da60004fa3b24e1ca1c7e93284f28597c968b8
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2024-04-06 11:45:23 +0100

    ensure create_category calls InMemoryCategoryRepository with correct values

--- src/core/category/application/create_category.py
+++ src/core/category/application/create_category.py
@@ -1,14 +1,21 @@
 from uuid import UUID
 
 from src.core.category.domain.category import Category
+from src.core.category.infra.in_memory_category_repository import InMemoryCategoryRepository
 
 class InvalidCategoryData(Exception):
     pass
 
 
-def create_category(name: str, description: str = "", is_active: bool = True) -> UUID:
+def create_category(
+    repository: InMemoryCategoryRepository,
+    name: str,
+    description: str = "", 
+    is_active: bool = True
+) -> UUID:
     try:
         category = Category(name, description, is_active)
     except ValueError as err:
         raise InvalidCategoryData(err)
+    repository.save(category)
     return category.id
--- src/core/category/tests/application/test_create_category.py
+++ src/core/category/tests/application/test_create_category.py
@@ -1,11 +1,16 @@
+from unittest.mock import MagicMock
 from uuid import UUID
 import pytest
 
 from src.core.category.application.create_category import InvalidCategoryData, create_category
+from src.core.category.infra.in_memory_category_repository import InMemoryCategoryRepository
+
+mock_repository = MagicMock(InMemoryCategoryRepository)
 
 class TestCreateCategory:
 	def test_create_category_with_valid_data(self):
 		category_id = create_category(
+			repository=mock_repository,
 			name="Filmes",
 			description="Categoria para filmes",
 			is_active=True
@@ -13,10 +18,12 @@ class TestCreateCategory:
 
 		assert category_id is not None
 		assert isinstance(category_id, UUID)
+		assert mock_repository.save.called
 
 	def test_create_category_with_invalid_data(self):
 		with pytest.raises(InvalidCategoryData, match="name cannot be empty") as exc_info:
 			category_id = create_category(
+				repository=mock_repository,
 				name=""
 			)
 

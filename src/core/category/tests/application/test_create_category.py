982fab820fefb86130c6a0dd27a6793cefd4bf34
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2024-04-06 12:17:03 +0100

    refactor change create_category usecase to use class CreateCategory

--- src/core/category/tests/application/test_create_category.py
+++ src/core/category/tests/application/test_create_category.py
@@ -2,30 +2,30 @@ from unittest.mock import MagicMock
 from uuid import UUID
 import pytest
 
-from src.core.category.application.create_category import InvalidCategoryData, create_category
+from src.core.category.application.create_category import CreateCategory, CreateCategoryRequest, InvalidCategoryData
 from src.core.category.infra.in_memory_category_repository import InMemoryCategoryRepository
 
-mock_repository = MagicMock(InMemoryCategoryRepository)
 
 class TestCreateCategory:
 	def test_create_category_with_valid_data(self):
-		category_id = create_category(
-			repository=mock_repository,
+		mock_repository = MagicMock(InMemoryCategoryRepository)
+		use_case = CreateCategory(repository=mock_repository)
+		request = CreateCategoryRequest(
 			name="Filmes",
 			description="Categoria para filmes",
 			is_active=True
 		)
+		
+		category_id = use_case.execute(request)
 
 		assert category_id is not None
 		assert isinstance(category_id, UUID)
-		assert mock_repository.save.called
+		assert mock_repository.save.called is True
 
 	def test_create_category_with_invalid_data(self):
 		with pytest.raises(InvalidCategoryData, match="name cannot be empty") as exc_info:
-			category_id = create_category(
-				repository=mock_repository,
-				name=""
-			)
+			use_case = CreateCategory(repository=MagicMock(InMemoryCategoryRepository))
+			category_id = use_case.execute(CreateCategoryRequest(name=""))
 
 		assert exc_info.type is InvalidCategoryData
 		assert str(exc_info.value) == "name cannot be empty"

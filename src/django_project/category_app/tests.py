44f225620d62cafca250bceea5990067682e5cb8
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2024-04-29 05:54:04 +0100

    test ensure CategoryViewSet returns correct values

--- src/django_project/category_app/tests.py
+++ src/django_project/category_app/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class TestCategoryAPI(APITestCase):
+    def test_list_category(self):
+        url = "/api/categories/"
+        response = self.client.get(url)
+
+        except_data = [
+            {
+                "id": "b5944983-7177-4a6e-a72a-26a9f330d20e",
+                "name": "Movie",
+                "description": "Movie Descriptin",  
+                "is_active": True
+            },
+            {
+                "id": "c06803a0-58a0-41bd-9094-b1f14a572e3f",
+                "name": "Documentary",
+                "description": "Documentary Descriptin",  
+                "is_active": True
+            }
+        ]
+
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.data, except_data)
+

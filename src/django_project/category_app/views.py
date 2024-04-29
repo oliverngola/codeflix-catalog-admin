4ac504b915d2730cadeb5e53f28af558ec781d93
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2024-04-29 11:43:02 +0100

    feat make CategoryView.list composition

--- src/django_project/category_app/views.py
+++ src/django_project/category_app/views.py
@@ -4,19 +4,27 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.status import HTTP_200_OK
 
+from src.core.category.application.use_cases.list_category import (
+    ListCategory, 
+    ListCategoryRequest
+)
+from django_project.category_app.repository import DjangoORMCategoryRepository
+
 class CategoryViewSet(viewsets.ViewSet):
-    def list(self, request:Request) -> Response:
-        return Response(status=HTTP_200_OK, data=[
+    def list(self, request: Request) -> Response:
+        input = ListCategoryRequest()
+
+        use_case = ListCategory(repository=DjangoORMCategoryRepository())
+        output = use_case.execute(input)
+
+        categories = [
             {
-                "id": "b5944983-7177-4a6e-a72a-26a9f330d20e",
-                "name": "Movie",
-                "description": "Movie Descriptin",  
-                "is_active": True
-            },
-            {
-                "id": "c06803a0-58a0-41bd-9094-b1f14a572e3f",
-                "name": "Documentary",
-                "description": "Documentary Descriptin",  
-                "is_active": True
+                "id": str(category.id),
+                "name": category.name,
+                "description": category.description,
+                "is_active": category.is_active,
             }
-        ])
+            for category in output.data
+        ]
+
+        return Response(status=HTTP_200_OK, data=categories)

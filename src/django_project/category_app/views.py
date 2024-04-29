672cbc68b8f69c5e90721839fd05dfd49f84fe39
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2024-04-29 15:35:31 +0100

    feat make CategoryView.retrieve composition

--- src/django_project/category_app/views.py
+++ src/django_project/category_app/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from uuid import UUID
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import (
+    HTTP_200_OK,
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND
+)
 
+from src.core.category.application.use_cases.exceptions import CategoryNotFound
+from src.core.category.application.use_cases.get_category import (
+    GetCategory,
+    GetCategoryRequest
+)
 from src.core.category.application.use_cases.list_category import (
     ListCategory, 
     ListCategoryRequest
@@ -28,3 +37,27 @@ class CategoryViewSet(viewsets.ViewSet):
         ]
 
         return Response(status=HTTP_200_OK, data=categories)
+    
+    def retrieve(self, request: Request, pk=None) -> Response:
+        try:
+            category_pk = UUID(pk)
+        except ValueError:
+            return Response(status=HTTP_400_BAD_REQUEST)
+        
+        use_case = GetCategory(repository=DjangoORMCategoryRepository())
+
+        try:
+            output = use_case.execute(GetCategoryRequest(id=category_pk))
+        except CategoryNotFound:
+            return Response(status=HTTP_404_NOT_FOUND)
+
+        category_output = {
+            "id": str(output.id),
+            "name": output.name,
+            "description": output.description,
+            "is_active": output.is_active,
+        }
+
+        return Response(status=HTTP_200_OK,data=category_output)
+
+

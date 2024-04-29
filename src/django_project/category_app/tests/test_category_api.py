0f0c1f0925f82ba62514554e7ab053b07db7db5e
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2024-04-29 11:43:20 +0100

    test make CategoryView.list composition

--- src/django_project/category_app/tests/test_category_api.py
+++ src/django_project/category_app/tests/test_category_api.py
@@ -1,23 +1,38 @@
-from django.test import TestCase
 from rest_framework.test import APITestCase
+
+from src.core.category.domain.category import Category
+from django_project.category_app.repository import DjangoORMCategoryRepository
+from django_project.category_app.models import Category as CategoryModel
+
 
 class TestCategoryAPI(APITestCase):
     def test_list_category(self):
+        category_filme = Category(
+            name="Filme",
+            description="Categoria para filmes",
+        )
+        category_serie = Category(
+            name="Série",
+            description="Categoria para séries",
+        )
+        repository = DjangoORMCategoryRepository(CategoryModel)
+        repository.save(category_filme)
+        repository.save(category_serie)
         url = "/api/categories/"
         response = self.client.get(url)
 
         except_data = [
             {
-                "id": "b5944983-7177-4a6e-a72a-26a9f330d20e",
-                "name": "Movie",
-                "description": "Movie Descriptin",  
-                "is_active": True
+                "id": str(category_filme.id),
+                "name": category_filme.name,
+                "description": category_filme.description,  
+                "is_active": category_filme.is_active
             },
             {
-                "id": "c06803a0-58a0-41bd-9094-b1f14a572e3f",
-                "name": "Documentary",
-                "description": "Documentary Descriptin",  
-                "is_active": True
+                "id": str(category_serie.id),
+                "name": category_serie.name,
+                "description": category_serie.description,  
+                "is_active": category_serie.is_active
             }
         ]
 

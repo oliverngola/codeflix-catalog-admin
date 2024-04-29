6f0e0c4bd0da7e8234243bd39a8a46ad23eb1fe0
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2024-04-29 15:35:45 +0100

    test make CategoryView.retrieve composition

--- src/django_project/category_app/tests/test_category_api.py
+++ src/django_project/category_app/tests/test_category_api.py
@@ -1,4 +1,3 @@
-from django.urls import reverse
 import pytest
 from rest_framework import status
 from rest_framework.test import APIClient
@@ -6,27 +5,27 @@ from src.core.category.domain.category import Category
 
 from src.django_project.category_app.repository import DjangoORMCategoryRepository
 
+@pytest.fixture
+def category_movie():
+    return  Category(
+        name="Movie",
+        description="Movie description",
+    )
+
+@pytest.fixture
+def category_documentary():
+    return Category(
+        name="Documentary",
+        description="Documentary description",
+    )
+
+@pytest.fixture
+def category_repository() -> DjangoORMCategoryRepository:
+    return DjangoORMCategoryRepository()
+
 
 @pytest.mark.django_db
 class TestCategoryAPI:
-    @pytest.fixture
-    def category_movie(self):
-        return  Category(
-            name="Movie",
-            description="Movie description",
-        )
-
-    @pytest.fixture
-    def category_documentary(self):
-        return Category(
-            name="Documentary",
-            description="Documentary description",
-        )
-
-    @pytest.fixture
-    def category_repository(self) -> DjangoORMCategoryRepository:
-        return DjangoORMCategoryRepository()
-
     def test_list_categories(
         self,
         category_movie: Category,
@@ -58,3 +57,49 @@ class TestCategoryAPI:
         assert len(response.data) == 2
         assert response.data == expected_data
 
+
+@pytest.mark.django_db
+class TestRetrieveAPi:
+    def test_when_id_is_invalid_return_400(
+        self,
+        category_movie: Category,
+        category_documentary: Category,
+        category_repository: DjangoORMCategoryRepository,
+    ) -> None:
+        url = f'/api/categories/1234567/'
+        response = APIClient().get(url)
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_return_category_when_exits(
+        self,
+        category_movie: Category,
+        category_documentary: Category,
+        category_repository: DjangoORMCategoryRepository,
+    ) -> None:
+        category_repository.save(category_movie)
+        category_repository.save(category_documentary)
+
+        url = f'/api/categories/{category_documentary.id}/'
+        response = APIClient().get(url)
+
+        expected_data = {
+            "id": str(category_documentary.id),
+            "name": "Documentary",
+            "description": "Documentary description",
+            "is_active": True
+        }
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == expected_data
+
+    def test_return_404_when_exits(
+        self,
+        category_movie: Category,
+        category_documentary: Category,
+        category_repository: DjangoORMCategoryRepository,
+    ) -> None:
+        url = f'/api/categories/{category_documentary.id}/'
+        response = APIClient().get(url)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND

1d2e9b4c39913fd2e60fb0bf8de3a59acfb19485
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2024-05-07 22:37:03 +0100

    test ensure CreateGenre raise exception if categories not found

---
+++ src/core/genre/tests/application/unit/test_unit_create_genre.py
@@ -0,0 +1,63 @@
+from unittest.mock import create_autospec
+import uuid
+import pytest
+
+from src.core.category.domain.category import Category
+from src.core.category.domain.category_repository import CategoryRepository
+from src.core.genre.application.use_cases.create_genre import CreateGenre
+from src.core.genre.application.use_cases.exceptions import RelatedCategoriesNotFound
+from src.core.genre.domain.genre_repository import GenreRepository
+
+
+@pytest.fixture
+def mock_genre_repository() -> GenreRepository:
+    return create_autospec(GenreRepository)
+
+
+@pytest.fixture
+def movie_category() -> Category:
+    return Category(name="Movie")
+
+
+@pytest.fixture
+def documentary_category() -> Category:
+    return Category(name="Documentary")
+
+@pytest.fixture
+def mock_category_repository_with_categories(
+    movie_category, 
+    documentary_category
+) -> CategoryRepository:
+    repository = create_autospec(CategoryRepository)
+    repository.list.return_value = [movie_category, documentary_category]
+    return repository
+
+@pytest.fixture
+def mock_empty_category_repository() -> CategoryRepository:
+    repository = create_autospec(CategoryRepository)
+    repository.list.return_value = []
+    return repository
+
+
+class TestCreateGenre:
+    def test_when_categories_do_not_exist_then_raise_related_categories_not_found(
+        self,
+        mock_empty_category_repository,
+        mock_genre_repository
+    ):
+        use_case = CreateGenre(
+            repository = mock_genre_repository,
+            category_repository = mock_empty_category_repository,
+        )
+        category_id = uuid.uuid4()
+        input = CreateGenre.Input(
+            name="Action", 
+            category_ids={category_id}
+        )
+        
+        with pytest.raises(RelatedCategoriesNotFound) as exc_info:
+            use_case.execute(input)
+
+        assert str(category_id)  in str(exc_info.value)
+
+    

31f0839f539a18e4b1a81792541bdf341ff67d11
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2024-05-07 22:36:16 +0100

    feat ensure CreateGenre raise exception if categories not found

---
+++ src/core/genre/domain/genre_repository.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+from uuid import UUID
+
+from src.core.genre.domain.genre import Genre
+
+
+class GenreRepository(ABC):
+    @abstractmethod
+    def get_by_id(self, id) -> Genre | None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def save(self, genre: Genre) -> None:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def delete(self, id: UUID) -> None:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def list(self) -> list[Genre]:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def update(self, genre: Genre) -> None:
+        raise NotImplementedError
+

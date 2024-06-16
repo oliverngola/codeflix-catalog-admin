d23bf59d06ba6be0eeadcfef3ab223f83913da8d
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2024-06-16 20:53:25 +0100

    feat ensure LocalStorage save files

---
+++ src/core/_shared/infrastructure/storage/local_storage.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from src.core._shared.infrastructure.storage.abstract_storage import AbstractStorage
+
+
+class LocalStorage(AbstractStorage):
+    TMP_BUCKET = "/tmp/codeflix-storege"
+
+    def __init__(self, bucket: str = TMP_BUCKET) -> None:
+        self.bucket = Path(bucket)
+
+        if not self.bucket.exists():
+            self.bucket.mkdir(parents=True)
+
+    def storage(self, file_path: str, content: bytes, content_type: str):
+        full_path = self.bucket / file_path
+
+        if not full_path.parent.exists():
+            full_path.parent.mkdir(parents=True)
+
+        with open(full_path, "wb") as file:
+            file.write(content)
